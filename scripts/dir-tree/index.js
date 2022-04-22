113e99b9fddc08fd58992b6e125753dcaa43890b
thomas-ballo <liurongliang@balloai.com>
2022-04-22 19:30:06 +0800

    chore: 把文件结构生成变成npm包

--- scripts/dir-tree/index.js
+++ scripts/dir-tree/index.js
@@ -1,50 +1,20 @@
-const fs = require('fs-extra')
-
+const mkdirTreeMd = require('mkdir-file-tree-md')
 const { resolve } = require('path')
 
-const ignore = [
-  'node_modules',
-  '.git',
-  '.DS_Store',
-  'dist',
-  '.temp',
-  '.vscode',
-  '.github'
-]
-
-const tree = []
-
-const readdir = (dirName, parentName, pre) => {
-  const filePath = `${parentName}/${dirName}`
-  let dirList = []
-  try {
-    dirList = fs.readdirSync(resolve(__dirname, filePath))
-  } catch (error) {}
-
-  if (dirList.length) {
-    for (let i = 0; i < dirList.length; i++) {
-      const newFileName = dirList[i]
-
-      if (!ignore.includes(newFileName)) {
-        const isLast = dirList.length - 1 === i
-        const preStr = isLast ? '└── ' : '├── '
-
-        tree.push(`${pre}${preStr}${newFileName}`)
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        readdir(newFileName, filePath, `·    ${pre}`)
-      }
-    }
-  }
-}
-
 async function run() {
-  readdir('..', '..', '')
-  console.log('tree', tree)
-
-  fs.writeFile(
-    resolve(__dirname, '../../DIR_TREE.md'),
-    ['```', ...tree, '```'].join('\n')
-  )
+  mkdirTreeMd({
+    entryDirPath: resolve(__dirname, '../..'),
+    treeFilePath: resolve(__dirname, '../../DIR_TREE.md'),
+    ignore: [
+      'node_modules',
+      '.git',
+      '.DS_Store',
+      'dist',
+      '.temp',
+      '.vscode',
+      '.github'
+    ]
+  })
 }
 
 run()
